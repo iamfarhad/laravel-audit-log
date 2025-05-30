382220fde20c467086f5b6d90fd09eb334814c87
farhadzand <f.zand@digikala.com>
2025-05-30 20:47:19 +0330

    remove facade helper

--- src/Facades/AuditLogger.php
+++ src/Facades/AuditLogger.php
@@ -6,17 +6,6 @@ namespace iamfarhad\LaravelAuditLog\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @method static void log(string $entityType, string|int $entityId, string $action, ?array $oldValues = null, ?array $newValues = null, array $metadata = [])
- * @method static void flush()
- * @method static array getLogsForEntity(string $entityType, string|int $entityId, array $options = [])
- * @method static void createStorageForEntity(string $entityClass)
- * @method static bool storageExistsForEntity(string $entityClass)
- * @method static void ensureStorageExists(string $entityClass)
- * @method static \iamfarhad\LaravelAuditLog\Contracts\AuditDriverInterface driver(?string $name = null)
- *
- * @see \iamfarhad\LaravelAuditLog\Services\AuditLogger
- */
 final class AuditLogger extends Facade
 {
     protected static function getFacadeAccessor(): string
