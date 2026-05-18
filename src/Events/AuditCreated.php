4e8dc8188dc0548e42d92043ef34f01e177fbf79
farhad zandmoghadam <farhad.pd@gmail.com>
2026-05-19 00:49:48 +0330

    Add audit lifecycle events

---
+++ src/Events/AuditCreated.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace iamfarhad\LaravelAuditLog\Events;
+
+use iamfarhad\LaravelAuditLog\Contracts\AuditLogInterface;
+
+final class AuditCreated
+{
+    public function __construct(public readonly AuditLogInterface $auditLog) {}
+}
